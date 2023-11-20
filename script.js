672fa26db14f7d6f24caac328d5e417112247aaf
youngpaxo <dacalderonnunez@gmail.com>
2023-11-20 00:00:52 -0300

    asd

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarAlertaPersonalizada() {
+    var overlay = document.getElementById('overlay');
+    overlay.style.display = 'flex';
+}
+
+function cerrarAlertaPersonalizada() {
+    var overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+}
